712923f5a28682d3d152349269f20fa8e32e2dbd
AlexLassooij <alex.lassooij@icloud.com>
2025-04-12 02:31:07 -0700

    added more epochs

--- film_output_cond.sh
+++ film_output_cond.sh
@@ -11,7 +11,7 @@ python pcnn_train.py \
 --lr_decay 0.999995 \
 --min_lr 0.00015 \
 --weight_decay 0.00001 \
---max_epochs 300 \
+--max_epochs 350 \
 --warmup_epochs 50 \
 --embedding_dim 16 \
 --model conditional \
